03b9317e71249763685057d3783d2fe3b455fdce
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2023-07-31 02:20:15 +0600

    added a gif in the pc-builder page

--- src/pages/pcBuilder.js
+++ src/pages/pcBuilder.js
@@ -1,15 +1,36 @@
 import React from "react";
 import RootLayout from "./components/Layout/RootLayout";
-import { Button, Card } from "antd";
+import { Button, Card, Image } from "antd";
 import Link from "next/link";
 const PcBuilderPage = ({ categories }) => {
   console.log(categories);
   return (
-    <div style={{ margin: "50px" }}>
-      <h1 style={{ textAlign: "center", margin: "5px" }}>
+    <div
+      style={{
+        margin: "50px",
+      }}
+    >
+      <Image
+        src="https://img.overclockers.co.uk/media/image/razerPCBSWORKSHOP.gif"
+        alt="news image"
+        height={300}
+        width={500}
+        style={{
+          marginLeft: "350px",
+          borderRadius: "10px",
+        }}
+      />
+      <h1 style={{ textAlign: "center", margin: "20px", fontSize: "30px" }}>
         PC Builder - Build Your Own Computer - CustomTech Wizardry
       </h1>
-      <Card style={{ margin: "50px" }}>
+      <Card
+        style={{
+          margin: "50px",
+          backgroundColor: "black",
+          color: "white",
+          fontWeight: "lighter",
+        }}
+      >
         {categories?.map((category) => (
           <div
             key={category.id} // Don't forget to include a unique key for each element in the array.
